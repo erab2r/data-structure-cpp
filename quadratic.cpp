1494c4b947f512f7c79f43e952975a343864c068
erab2r <shkera213@gmail.com>
2025-06-17 16:31:55 +0600

    ,,,

---
+++ quadratic.cpp
@@ -0,0 +1,27 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+//     int n;
+//     cin >> n;
+//     for(int i = 1; i <= n; i++) {
+//        for(int i = 1; i <= n; i++) {
+//        cout << "HELLO" <<endl;
+//     }
+//     }
+//     return 0;
+// }
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n,m;
+    cin >> n >> m;
+    for(int i = 1; i <= n; i++) {
+       for(int i = 1; i <= m; i++) {
+       cout << "HELLO" <<endl;
+    }
+    }
+    return 0;
+}
