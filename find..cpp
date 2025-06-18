ef62c0f9d545dfd1e5515dcb7f5e3828cb4cad8a
erab2r <shkera213@gmail.com>
2025-06-18 22:01:42 +0600

    kfghj

---
+++ find..cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    vector<int>v = {1,2,3,3,5,3};
+    auto it = find(v.begin(),v.end(),300);
+
+    if(it== v.end()){
+        cout << "Not found" << " ";
+    }
+    else{
+        cout << "Found" << " ";
+    }
+    return 0;
+}
