f13be1628164993a7b455b427c206c22c935a9d7
erab2r <shkera213@gmail.com>
2025-06-18 21:35:31 +0600

    klmb

--- insert.cpp
+++ insert.cpp
@@ -3,7 +3,8 @@ using namespace std;
 
 int main() {
     vector<int>v = {1,2,3,4,5,6};
-    v.insert(v.begin()+2,100);
+    vector<int>v2 = {200,300,900,457};
+    v.insert(v.begin()+2,v2.begin(),v2.end());
 
     for(int x : v){
         cout << x << " ";
