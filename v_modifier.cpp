4a2320c7f8b15e8a35f329aae583998832c02839
erab2r <shkera213@gmail.com>
2025-06-18 16:25:06 +0600

    [];

--- v_modifier.cpp
+++ v_modifier.cpp
@@ -5,10 +5,11 @@ int main() {
     vector<int>v = {1,2,3,4,5,6};
     vector<int>v2;
     v2 = v;
-    for(int i=0;i<v2.size();i++){
-        cout << v2[i] <<" ";
+    // for(int i=0;i<v2.size();i++){
+    //     cout << v2[i] <<" ";
+    // }
+    for(int x : v2){
+        cout << x << " ";
     }
-
-
     return 0;
 }
