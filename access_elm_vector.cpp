4546cc41632480306d91c1ec630ac6ece092515d
erab2r <shkera213@gmail.com>
2025-06-20 13:35:00 +0600

    jj

--- access_elm_vector.cpp
+++ access_elm_vector.cpp
@@ -3,8 +3,8 @@ using namespace std;
 
 int main() {
     vector<int>v = {1,2,3,3,5,8};
-//    cout << v[3] << " ";
+      cout << v[3] << " ";
       cout << v.back() << " ";
-     cout << v.front() << " ";
+      cout << v.front() << " ";
     return 0;
 }
