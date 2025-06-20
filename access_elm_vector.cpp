aae4067ebcb18d48efa70be0dabca39cf803b390
erab2r <shkera213@gmail.com>
2025-06-20 13:29:59 +0600

    y

---
+++ access_elm_vector.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    vector<int>v = {1,2,3,3,5,8};
+//    cout << v[3] << " ";
+      cout << v.back() << " ";
+     cout << v.front() << " ";
+    return 0;
+}
