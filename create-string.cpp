f94b2ae9fd6d6f9d8f210d233da78ffbac765cdc
erab2r <shkera213@gmail.com>
2025-06-22 22:24:56 +0600

    gdfgfd

---
+++ create-string.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    vector<string>words(2);
+    cin >> words[0] >> words[1];
+    cout << words[0].length() << " " <<words[1].length() <<endl;
+    cout << words[0] <<" " <<words[1] <<endl;
+
+    return 0;
+}
